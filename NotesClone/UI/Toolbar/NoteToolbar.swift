9cdb87384cbe3d93f2a4d2d3ab336f5df0d62b6c
Celine <s.kayar@yahoo.com>
2024-07-27 19:43:55 +0300

    Fixed: Replaced the custom toolbar view with a UIToolbar for the UITextView. I was unaware that UIToolbar exitsted, and that it could be used directly as an accessory view, which simplifies handling the toolbar's visibility with respect to keyboard events.

---
+++ NotesClone/UI/Toolbar/NoteToolbar.swift
@@ -0,0 +1,20 @@
+//
+//  NoteToolbar.swift
+//  NotesClone
+//
+//  Created by Selin Kayar on 27.07.24.
+//
+
+import UIKit
+
+class NoteToolbar: UIToolbar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
